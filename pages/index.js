0174c43f37cbd8bfce4e31dc64ef56860bd100ab
mehulbigfaida-glitch <mehul.bigfaida@gmail.com>
2025-11-03 00:36:53 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import Hero from '@/components/Hero';
-import Showcase from '@/components/Showcase';
-import HowItWorks from '@/components/HowItWorks';
-import Footer from '@/components/Footer';
+// pages/index.js
+import Hero from "../components/Hero";
+import Showcase from "../components/Showcase";
+import HowItWorks from "../components/HowItWorks";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
